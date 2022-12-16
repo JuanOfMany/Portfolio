6977333892dba5d32007e1e45b6febaddb9557d8
Juan Pinol <JuannnCodes@gmail.com>
2022-12-15 19:35:35 -0500

    Changed button classNames and added CSS to start flexbox grid implementation.

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -9,24 +9,24 @@ import {
 export default function SideBar () {
 
   return (
-    <div id="sidebar">
-    <div>
-      <h1>Juan Pinol</h1>
-    </div>
-    <div>
-    <RouterLink to="/">
-      <button>Home</button>
-    </RouterLink>
-    <RouterLink to="/miata">
-      <button>Miata</button>
-    </RouterLink>
-    <RouterLink to="/aboutme">
-      <button>About Me</button>
-    </RouterLink>
-    <RouterLink to="/techstack">
-      <button>Tech Stack</button>
-    </RouterLink>
-    </div>
+    <div id="sidebar" className="flex-grid">
+      <div>
+        <h1>Juan Pinol</h1>
+      </div>
+      <div>
+      <RouterLink to="/">
+        <button className="col">Home</button>
+      </RouterLink>
+      <RouterLink to="/miata">
+        <button className="col">Miata</button>
+      </RouterLink>
+      <RouterLink to="/aboutme">
+        <button className="col">About Me</button>
+      </RouterLink>
+      <RouterLink to="/techstack">
+        <button className="col">Tech Stack</button>
+      </RouterLink>
+      </div>
   </div>
   )
 }
